a7164e6810be984a98eef6f323cacb5e4794f7cb
0xsign <0xsign@protonmail.com>
2022-10-05 16:31:54 +0200

    use webpack to includes json files at compile time

--- index.ts
+++ index.ts
@@ -1,30 +1,33 @@
-import fs from "fs";
-import path from "path";
+// Read all files from "labels" folder using webpack
+// and create a mapping of address - labels
+function requireAll(requireContext: any) {
+  const keys = requireContext.keys();
+  const values = keys.map(requireContext);
+  const registry: { [chain: string]: Label } = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    const addressWithoutExtension = keys[i].split(".json")[0];
+    const addressWithoutSlashes = addressWithoutExtension.split("/");
+    const address = addressWithoutSlashes[addressWithoutSlashes.length - 1];
+
+    registry[address] = values[i];
+  }
+
+  return registry;
+}
+
+const registry: { [key: string]: Label } = requireAll(
+  require.context("./labels", false, /.json$/)
+);
 
 export interface Label {
   labels: string[];
   links: { [key: string]: string };
 }
 
-const registries: { [chain: string]: Label } = {};
-
-const filenames = fs.readdirSync(path.join(__dirname, "labels"));
-
-for (const filename of filenames) {
-  const buff = fs.readFileSync(
-    path.join(__dirname, "labels", filename),
-    "utf8"
-  );
-  const label = JSON.parse(buff);
-
-  const address = filename.split(".")[0];
-
-  registries[address] = label;
-}
-
 /**
  * @param address lowercase hex string. ex: "0x0000000000000000000000000000000000000000"
  */
 export function getLabel(address: string) {
-  return registries[address];
+  return registry[address];
 }
